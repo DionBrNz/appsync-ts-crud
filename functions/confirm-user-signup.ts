26f456b844ce55d47c3c99518029f9b7ec39f0e9
Dion Brown <37971358+DionBrNz@users.noreply.github.com>
2021-08-08 20:59:58 +0000

    Typescript working Added Cognito and User table.

---
+++ functions/confirm-user-signup.ts
@@ -0,0 +1,33 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { PostConfirmationConfirmSignUpTriggerEvent } from "aws-lambda/trigger/cognito-user-pool-trigger";
+import { User } from "../lib/entities";
+
+const { USERS_TABLE } = process.env
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocument = DynamoDBDocument.from(dynamoClient);
+
+export async function handler(
+  event: PostConfirmationConfirmSignUpTriggerEvent,
+  contex: any
+): Promise<PostConfirmationConfirmSignUpTriggerEvent> {
+  
+
+  if (event.triggerSource == "PostConfirmation_ConfirmSignUp") {
+    const user: User = {
+      id: event.userName,
+      email: event.request.userAttributes["email"],
+      created: new Date().toISOString(),
+    };
+
+    await dynamoDocument.put({
+      TableName: USERS_TABLE,
+      Item: user,
+      ConditionExpression: "attribute_not_exists(id)",
+    });
+
+    return event;
+  } else {
+    return event;
+  }
+}
