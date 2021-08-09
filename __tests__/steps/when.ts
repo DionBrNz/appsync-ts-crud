483b1ca13ebc82df82c9288fe72e66fa0f08a2e6
Dion Brown <dionbr@outlook.com>
2021-08-09 17:33:49 +1200

    Started adding tests and got export-env working

---
+++ __tests__/steps/when.ts
@@ -0,0 +1,32 @@
+import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider'
+import { PostConfirmationConfirmSignUpTriggerEvent } from 'aws-lambda'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const { AWS_REGION, WEB_USER_POOL_CLIENT_ID, COGNITO_USER_POOL_ID } = process.env
+const cognito = new CognitoIdentityProvider({})
+
+export interface ICognitoUser {
+  id: string
+  email: string
+}
+export async function a_user_signs_up(email: string, password: string): Promise<ICognitoUser> {
+  const { UserSub: id } = await cognito.signUp({
+    Username: email,
+    Password: password,
+    ClientId: WEB_USER_POOL_CLIENT_ID
+  })
+
+  expect(id).toBeTruthy()
+
+  console.log({ email, id }, 'User has signed up')
+
+  await cognito.adminConfirmSignUp({
+    UserPoolId: COGNITO_USER_POOL_ID,
+    Username: email
+  })
+
+  console.log({ email, id }, 'Confirmed sign up')
+
+  return { id: id!, email: email }
+}
