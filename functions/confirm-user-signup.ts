153bf5956cbf5ea4b23ce960e10631f75f9b22e3
Dion Brown <37971358+DionBrNz@users.noreply.github.com>
2021-08-10 03:25:48 +0000

    Added pino-lambda. Use the lambda Context type.

--- functions/confirm-user-signup.ts
+++ functions/confirm-user-signup.ts
@@ -1,32 +1,37 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import { PostConfirmationConfirmSignUpTriggerEvent } from "aws-lambda/trigger/cognito-user-pool-trigger";
-import { User } from "../lib/entities";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { PostConfirmationConfirmSignUpTriggerEvent } from 'aws-lambda/trigger/cognito-user-pool-trigger'
+import { User } from '../lib/entities'
+import lambdaLogger from 'pino-lambda'
+import { Context } from 'aws-lambda'
 
 const { USERS_TABLE_NAME } = process.env
-const dynamoClient = new DynamoDBClient({});
-const dynamoDocument = DynamoDBDocument.from(dynamoClient);
+const dynamoClient = new DynamoDBClient({})
+const dynamoDocument = DynamoDBDocument.from(dynamoClient)
+const logger = lambdaLogger()
 
 export async function handler(
   event: PostConfirmationConfirmSignUpTriggerEvent,
-  contex: any
+  contex: Context
 ): Promise<PostConfirmationConfirmSignUpTriggerEvent> {
   
-  if (event.triggerSource == "PostConfirmation_ConfirmSignUp") {
+  logger.withRequest(event, contex)
+  if (event.triggerSource == 'PostConfirmation_ConfirmSignUp') {
     const user: User = {
       id: event.userName,
-      email: event.request.userAttributes["email"],
-      created: new Date().toISOString(),
-    };
+      email: event.request.userAttributes['email'],
+      created: new Date().toISOString()
+    }
 
     await dynamoDocument.put({
       TableName: USERS_TABLE_NAME,
       Item: user,
-      ConditionExpression: "attribute_not_exists(id)",
-    });
+      ConditionExpression: 'attribute_not_exists(id)'
+    })
+    logger.info('Created user')
 
-    return event;
+    return event
   } else {
-    return event;
+    return event
   }
 }
